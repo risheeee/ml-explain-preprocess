4b7110db46a9d00d393e2b9adbbd693810233261
unknown <rishrash2712@gmail.com>
2025-09-08 22:45:09 +0530

    build changes

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 import numpy as np
-from ml_explain_preprocess import (
+from src.ml_explain_preprocess import (
     explain_fill_missing,
     explain_encode,
     explain_scale,
